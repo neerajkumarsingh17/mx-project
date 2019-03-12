1a5e84120413eb08ef26be9f8591db8ea1a0d459
deepakbartwal <deepaksinghbartwal.com>
2019-03-12 18:29:52 +0530

    add some post related code

--- urls.py
+++ urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.course_date, name='course_date'),
-    url(r'^enrolled_data/', views.enrolled_data, name='enrolled_data'),
+   # url(r'^enrolled_data/', views.enrolled_data, name='enrolled_data'),
 
 
    
 ]
-
+        
 
 
 
--- views.py
+++ views.py
@@ -5,26 +5,26 @@ from django.contrib.auth.models import User
 from enrollment.api import add_enrollment
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseRedirect
+
 
 def course_date(request):
+  submitted = False
   if request.method=="POST":
-
     selected_option = request.POST.get('my_options', None)
-    if selected_option:
-      enrolled_course = request.POST.get('enroll_course')
-    selected_option_user = request.POST.get('my_options_user', None)
-    if selected_option:
-      enrolled_user = request.POST.get('enrolled_user')
-
+    #selected_option_1 = request.POST.get('my_options_1', None)
+    #if selected_option:
+      #if selected_option.is_valid():
+    return HttpResponseRedirect('/enrolled_data?submitted=True')
     
   else:
     course = CourseOverview.objects.only('id')
     users = User.objects.only('username')
-    context = 
-    {   'course': course,
+    context = { 
+        'course': course,
         'users': users
     }
-  return render(request, "mx-enrollment_new/mx-enrollment_new.html",context)
+  return render(request, "mx-enrollment_new/mx-enrollment_new.html", context)
 
 
 """ 
@@ -48,6 +48,22 @@ def enrolled_data(request):
       'enrolled_user': enrolled_user
   })
  """
+
+
+""" def course_date(request):
+  submitted = False
+  if request.method == "POST":
+    selected_option = request.POST.get('my_options', None)
+    if selected_option:
+      if selected_option.is_valid():
+        enrolled_course = request.POST.get('enroll_course')
+        selected_option.save()
+    selected_option_user = request.POST.get('my_options_user', None)
+    if selected_option:
+      if selected_option.is_valid():
+        enrolled_user = request.POST.get('enrolled_user')
+        selected_option.save()
+        return HttpResponseRedirect('/enrolled_data?submitted=True') """
 
 
 
