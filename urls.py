772d8686cf008a17ecbecfb6c7e3764e58f2201a
neerajkumarsingh17 <47746422+neerajkumarsingh17@users.noreply.github.com>
2019-04-16 17:43:55 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,9 +3,7 @@ from . import views
 
 
 urlpatterns = [
-
-    url(r'^$', views.course_date, name='course_date'),
+    
     url(r'^enroll_user/', views.enroll_user, name='enroll_user'),
     url(r'^check_status/', views.check_status, name='check_status'),
-    # url(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status')
 ]
