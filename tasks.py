aca3c0956c8dec65d4d1d8bc77de7833970b9d51
neerajkumarsingh17 <47746422+neerajkumarsingh17@users.noreply.github.com>
2019-04-16 17:42:55 +0530

    finally

--- tasks.py
+++ tasks.py
@@ -2,31 +2,75 @@ from __future__ import absolute_import
 
 from enrollment.api import add_enrollment, get_enrollment
 from celery.task import task
-from celery import current_task
+from django.contrib.auth.models import User
 from celery.result import AsyncResult
 
 
 @task
 def enrollment_task(user, course):
-    user_id = user
-    len_of_user = len(user_id)
-    print('User Length : ', len_of_user)
     course_id = course
-    count = 0
-    for i, user_name in enumerate(user_id):
-        try:
-            if not get_enrollment(user_name, course_id):
+    users = user
+    print(enrollment_task.request.id)
+    #users_len -> count all users length 
+    users_len = (User.objects.count())
+    print('total users len count :--',users_len)
+    if not users :
+        i = 100
+        count = 0
+        cou = 0
+        while True:
+            users = User.objects.values_list('username', flat=True)[count:i]
+            for user_name in users:
                 try:
-                    add_enrollment(user_name, course_id, is_active=True)
-                except:
-                    pass
-                else:
-                    count += 1
-                    print('user {0} is enroll in this course {1}'.format(user_name, course_id))
-                    print('User Number------------->', count)
-            else:
-                print('user {0} is already enroll in this course {1}'.format(user_name, course_id))
-                pass
-        except Exception as e:
-            print(e)
+                    if not get_enrollment(user_name, course_id):
+                        try:
+                            add_enrollment(user_name, course_id, is_active=True)
+                            cou  += 1
+                        except:
+                            pass
+                        else:
+                            print('user {0} is enroll in this course {1}'.format(user_name, course_id))
+                    else:
+                        cou  += 1
+                        print('user {0} is already enroll in this course {1}'.format(user_name, course_id))
+                        print(cou)
+                        pass
+                except Exception as e:
+                    print(e)
+            len_user = cou
+            if (users_len == cou):
+                break        
+            count = i
+            i += 100       
+    else:                
+        len_user = len(users) 
+        i = 1
+        count = 0
+        cou = 0
+        while True:
+            selected_users = users[count:i]   
+            for user_name in selected_users:
+                try:
+                    if not get_enrollment(user_name, course_id):
+                        try:
+                            add_enrollment(user_name, course_id, is_active=True)
+                            cou  += 1
+                        except:
+                            pass
+                        else:
+                            print('user {0} is enroll in this course {1}'.format(user_name, course_id))
+                    else:
+                        print('user {0} is already enroll in this course {1}'.format(user_name, course_id))
+                        cou  += 1
+                        pass
+                except Exception as e:
+                    print(e)
+            if (len_user == cou):
+                break
+            count = i
+            i += 1
     return ("Enrollment complete")
+
+
+
+    
